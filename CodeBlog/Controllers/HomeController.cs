313f545d79ef3c30606c20b7983951d74bed9c9b
Tấn Thanh <tanthanh141214@gmail.com>
2020-06-05 16:29:05 +0700

    Add project files.

---
+++ CodeBlog/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult CodeNoiBat()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CodeBlog/Controllers/TaiKhoanController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeBlog.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        // GET: TaiKhoan
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangKy1()
+        {
+            return View();
+        }
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(string txtTaiKhoan, string txtMatKhau)
+        {
+            return View();
+        }
+    }
+}
